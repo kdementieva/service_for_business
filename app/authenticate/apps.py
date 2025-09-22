deee4625c07d7a26d22a2ebf6f50060a320be4df
Kateryna Dementieva <katerynadementieva@gmail.com>
2025-09-22 08:47:59 +0200

    startapp authenticate

---
+++ app/authenticate/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class AuthenticateConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authenticate'
+    verbose_name = 'User'
+    verbose_name_plural = 'User'
